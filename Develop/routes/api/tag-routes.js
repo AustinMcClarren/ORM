4b18a1ab320060964cf81a21d65dafe063b48147
Austin McClarren <austinmcclarren7@gmail.com>
2023-02-16 13:50:47 -0500

    finished tag routes and did the rest of models, still working on product routes

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -28,14 +28,18 @@ router.get('/:id', (req, res) => {
 
 // create a new tag
 router.post('/', (req, res) => {
-
+Tag.create({Tag_name:req.body.Tag_name})
+.then(tagData=> res.status(200).json(tagData))
+.catch(err=>  res.status(400).json(err))
 });
 
 
 
 // update a tag's name by its `id` value
 router.put('/:id', (req, res) => {
-
+  Tag.update(req.body,{where:{id:req.params.id}})
+  .then(tagUpdate=> res.status(200).json(tagUpdate))
+  .catch(err=> res.status(400).json(err))
 });
 
 
@@ -43,7 +47,9 @@ router.put('/:id', (req, res) => {
 
 // delete on tag by its `id` value
 router.delete('/:id', (req, res) => {
-
+  Tag.delete({where:{id:req.params.id}})
+  .then(TagDelete=> res.status(200).json(TagDelete))
+  .catch(err=> res.status(400).json(err))
 });
 
 module.exports = router;
